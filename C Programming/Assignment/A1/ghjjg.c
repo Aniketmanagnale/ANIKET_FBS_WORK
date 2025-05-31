a39bbd9b89937c7f4eb28f1ebbf26ab54f940641
Aniketmanagnale <153374329+Aniketmanagnale@users.noreply.github.com>
2025-05-31 15:08:08 +0530

    Add files via upload

---
+++ C Programming/Assignment/A1/ghjjg.c
@@ -0,0 +1,13 @@
+#include<stadio.h>
+void main(){
+    char mess[100]
+    int i=1;
+    printf("enter your massage;");
+scanf("%s",&mess);
+do{
+    printf("%s\n",mess);
+    i++
+
+}
+while(i<=10);
+}
